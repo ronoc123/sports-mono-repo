855613faebbcacbc35e627dad7054ef0d2a45ac1
Conor Kamperman <kamperman.conor@gmail.com>
2025-09-20 20:04:28 -0500

    updates

--- libs/auth/auth-data-access/src/auth.model.ts
+++ libs/auth/auth-data-access/src/auth.model.ts
@@ -1,16 +1,14 @@
-
-
 export type AuthState = {
   loggedIn: boolean;
   user: any;
 };
 
 export const initialUserValue: any = {
-  email: '',
-  username: '',
-  password: '',
-  bio: '',
-  image: '',
+  email: "",
+  username: "",
+  password: "",
+  bio: "",
+  image: "",
 };
 
 export const authInitialState: AuthState = {
